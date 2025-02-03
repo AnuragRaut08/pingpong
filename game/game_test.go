a2d540e8ad60f59344d2c0d90274c0f2a8f61ba9
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-02-03 11:26:04 +0530

    Update game_test.go

--- game/game_test.go
+++ game/game_test.go
@@ -1,18 +1,25 @@
 package game
 
 import (
-	"pingpong/player"
 	"testing"
+	"pingpong/player"
 )
 
+// TestPlayGame ensures that the game plays until a winner is determined
 func TestPlayGame(t *testing.T) {
-	player1 := &player.Player{Name: "Rishi", DefenseLength: 8}
-	player2 := &player.Player{Name: "Suraj", DefenseLength: 7}
+	player1 := &player.Player{Name: "Rishi", DefenseLength: 7}
+	player2 := &player.Player{Name: "Suraj", DefenseLength: 6}
+
 	game := Game{Player1: player1, Player2: player2}
 
-	winner := game.PlayGame()
+	// Just call the function, don't assign it to a variable
+	game.PlayGame()
 
-	if winner != "Rishi" && winner != "Suraj" {
-		t.Errorf("Invalid winner: got %s", winner)
+	if game.Winner == nil {
+		t.Errorf("Expected a winner, but got nil")
+	}
+
+	if game.Winner.Score < 5 {
+		t.Errorf("Expected winner to have at least 5 points, but got %d", game.Winner.Score)
 	}
 }
